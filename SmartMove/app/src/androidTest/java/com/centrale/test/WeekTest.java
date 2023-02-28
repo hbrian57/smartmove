172ccacf6c0da70f286e503ecc2efa7809262c3f
Mael Tudal <johndoe@example.com>
2023-02-28 16:04:28 +0100

    Rajout d'un test sur la Week

--- SmartMove/app/src/androidTest/java/com/centrale/test/WeekTest.java
+++ SmartMove/app/src/androidTest/java/com/centrale/test/WeekTest.java
@@ -67,5 +67,56 @@ public class WeekTest {
     }
 
 
+    @Test
+    public void testGetTotalCO2FootprintCaseRandom() throws Exception {
+        //Le voyage 1
+        LinkedList<TimestampedPosition> bikePositions1 = new LinkedList<>();
+        bikePositions1.add(new TimestampedPosition(0, 0, 0));
+        bikePositions1.add(new TimestampedPosition(1, 1, 0));
+        TripSegment tripsegm1 = new TripSegment(TransportType.BIKE, bikePositions1);
+
+        //Le voyage 2
+        LinkedList<TimestampedPosition> carPositions1 = new LinkedList<>();
+        carPositions1.add(new TimestampedPosition(0, 0, 0));
+        carPositions1.add(new TimestampedPosition(1, 0, 0));
+        TripSegment tripsegm2 = new TripSegment(TransportType.CAR, carPositions1);
+
+        //Un trip composé des deux voyages
+        ArrayList<TripSegment> tripSegments1 = new ArrayList<>();
+        tripSegments1.add(tripsegm1);
+        tripSegments1.add(tripsegm2);
+        Trip trip1= new Trip(tripSegments1);
+
+
+        //Le voyage 3
+        LinkedList<TimestampedPosition> bikePositions2 = new LinkedList<>();
+        bikePositions2.add(new TimestampedPosition(0, 0, 0));
+        bikePositions2.add(new TimestampedPosition(1, 1, 0));
+        TripSegment tripsegm3 = new TripSegment(TransportType.BIKE, bikePositions2);
+
+        //Le voyage 4
+        LinkedList<TimestampedPosition> carPositions2 = new LinkedList<>();
+        carPositions2.add(new TimestampedPosition(0, 0, 0));
+        carPositions2.add(new TimestampedPosition(1, 0, 0));
+        TripSegment tripsegm4 = new TripSegment(TransportType.CAR, carPositions2);
+
+        //Un trip composé des deux voyages
+        ArrayList<TripSegment> tripSegments2 = new ArrayList<>();
+        tripSegments1.add(tripsegm3);
+        tripSegments1.add(tripsegm4);
+        Trip trip2= new Trip(tripSegments2);
+
+        //Création de la week
+        ArrayList trips = new ArrayList<Trip>();
+        trips.add(trip1);
+        trips.add(trip2);
+        Week week = new Week(trips);
+
+
+        // call la methode et vérification du résultat
+        double expected = 1269198;
+        double actual = week.getTotalCO2Footprint();
+        assertEquals(expected, actual, 10);
+    }
 
 }
