cc39a021d03d53adb1d41681ce5b1a7eb4868e40
Mael Tudal <johndoe@example.com>
2023-03-23 16:40:09 +0100

    Modifications ChallengeGenerator

--- SmartMove/app/src/main/java/com/centrale/smartmove/ChallengeGenerator.java
+++ SmartMove/app/src/main/java/com/centrale/smartmove/ChallengeGenerator.java
@@ -1,9 +1,20 @@
 package com.centrale.smartmove;
 
+import android.content.res.Resources;
+
 import com.centrale.smartmove.models.Challenge;
+import com.centrale.smartmove.models.Equivalent;
 import com.centrale.smartmove.models.GoalType;
 
-import java.util.Date;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class ChallengeGenerator {
@@ -21,11 +32,17 @@ public class ChallengeGenerator {
      * @return Challenge challenge
      */
     public Challenge getRandomChallenge(){
+
+        //Sélection du challenge
         Challenge challenge = new Challenge();
-        // Goal
-        Random random = new Random();
-        int result = random.nextInt(2);// BOUND A CHANGER EN FONCTION DU NBR DES DEFIS
-        if (result ==0){
+        ArrayList challenges = challenge.creationOfTheChallengeList();
+        Random rand = new Random();
+        int index = rand.nextInt(challenges.size());
+        return (Challenge) challenges.get(index);
+
+// goal type dans le json et transport ausis. Faire la boucle if
+    // Goal
+        if (int result ==0){
             GoalType goalType = GoalType.NUMBER_OF_TRIPS;// A CHANGER EN FONCTION DES DEFIS
             challenge.getGoal().setType(goalType);
         }
@@ -33,18 +50,25 @@ public class ChallengeGenerator {
             GoalType goalType2 =GoalType.DISTANCE_COVERED;// A CHANGER EN FONCTION DES DEFIS
             challenge.getGoal().setType(goalType2);
         }
+
+        //en foinction du type et d transport type, faire des fourchettes d'objectifs.
         challenge.getGoal().setGoalFinal(5.0);// A VERIFIER
         // progression
         challenge.setProgression(70.0);
 
         // Title A FAIRE EN FONCTION DES DEFIS
-        challenge.setTitle(R.string.challenge_title);
+        challenge.setTitle(challenge.getTitle());
 
         //Description A FAIRE EN FONCTION DES DEFIS
-        challenge.setDescription(R.string.challenge_description);
+        challenge.setShort_description(challenge.getShort_description());
+
+        //Description A FAIRE EN FONCTION DES DEFIS
+        challenge.setLong_description(challenge.getLong_description());
 
         return challenge;
     }
+
+
 
     //----------------------------------------------------------------------------------------------
 }
